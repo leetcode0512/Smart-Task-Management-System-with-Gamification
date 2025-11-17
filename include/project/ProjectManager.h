41a5c0a1ea2b40df5fb7efb9dc177ec68d921616
RONG678 <rong67888@gmail.com>
2025-11-17 20:11:15 +0800

    Add files via upload

--- include/project/ProjectManager.h
+++ include/project/ProjectManager.h
@@ -1,31 +1,35 @@
 #ifndef PROJECT_MANAGER_H
 #define PROJECT_MANAGER_H
 
-#include "Project.h"
+#include "entities/Project.h"
+#include "dao/ProjectDAO.h"
 #include <vector>
-#include <map>
+#include <string>
+
+using namespace std;
 
 class ProjectManager {
 private:
-    // 暂时用map存储，后续替换成数据库
-    map<int, Project*> projects;
-    int nextId;
+    ProjectDAO* dao;
 
 public:
     ProjectManager();
+    ProjectManager(string dbPath);
     ~ProjectManager();
     
-    // CRUD操作
+    bool initialize();
+    
     int createProject(const Project& project);
     Project* getProject(int id);
     vector<Project*> getAllProjects();
+    vector<Project*> getAllProjectsIncludingArchived();
     bool updateProject(const Project& project);
     bool deleteProject(int id);
     
-    // 业务功能
     double calculateProgress(int project_id);
     void updateProjectProgress(int project_id);
     int getProjectCount();
+    int getActiveProjectCount();
 };
 
 #endif
